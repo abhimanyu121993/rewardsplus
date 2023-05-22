79274ae252775eb9b0f34b66a8b6b00d5fd97c22
Abhimanyu <abhimanyu121993@gmail.com>
2023-05-22 10:40:22 +0530

    db

---
+++ database/reward_old.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 22, 2023 at 07:05 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `reward_old`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendence`
+--
+-- Error reading structure for table reward_old.attendence: #1932 - Table 'reward_old.attendence' doesn't exist in engine
+-- Error reading data for table reward_old.attendence: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `reward_old`.`attendence`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+-- Error reading structure for table reward_old.company: #1932 - Table 'reward_old.company' doesn't exist in engine
+-- Error reading data for table reward_old.company: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `reward_old`.`company`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+-- Error reading structure for table reward_old.employee: #1932 - Table 'reward_old.employee' doesn't exist in engine
+-- Error reading data for table reward_old.employee: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `reward_old`.`employee`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `store`
+--
+-- Error reading structure for table reward_old.store: #1932 - Table 'reward_old.store' doesn't exist in engine
+-- Error reading data for table reward_old.store: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `reward_old`.`store`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ database/rewardsplus_2.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 22, 2023 at 07:05 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rewardsplus_2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+-- Error reading structure for table rewardsplus_2.admins: #1932 - Table 'rewardsplus_2.admins' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.admins: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`admins`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `analytics_configs`
+--
+-- Error reading structure for table rewardsplus_2.analytics_configs: #1932 - Table 'rewardsplus_2.analytics_configs' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.analytics_configs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`analytics_configs`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business_types`
+--
+-- Error reading structure for table rewardsplus_2.business_types: #1932 - Table 'rewardsplus_2.business_types' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.business_types: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`business_types`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companies`
+--
+-- Error reading structure for table rewardsplus_2.companies: #1932 - Table 'rewardsplus_2.companies' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.companies: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`companies`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company_categories`
+--
+-- Error reading structure for table rewardsplus_2.company_categories: #1932 - Table 'rewardsplus_2.company_categories' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.company_categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`company_categories`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company_details`
+--
+-- Error reading structure for table rewardsplus_2.company_details: #1932 - Table 'rewardsplus_2.company_details' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.company_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`company_details`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+-- Error reading structure for table rewardsplus_2.employees: #1932 - Table 'rewardsplus_2.employees' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.employees: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`employees`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table rewardsplus_2.failed_jobs: #1932 - Table 'rewardsplus_2.failed_jobs' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`failed_jobs`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table rewardsplus_2.migrations: #1932 - Table 'rewardsplus_2.migrations' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`migrations`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `model_has_permissions`
+--
+-- Error reading structure for table rewardsplus_2.model_has_permissions: #1932 - Table 'rewardsplus_2.model_has_permissions' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.model_has_permissions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`model_has_permissions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `model_has_roles`
+--
+-- Error reading structure for table rewardsplus_2.model_has_roles: #1932 - Table 'rewardsplus_2.model_has_roles' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.model_has_roles: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`model_has_roles`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+-- Error reading structure for table rewardsplus_2.password_reset_tokens: #1932 - Table 'rewardsplus_2.password_reset_tokens' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.password_reset_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`password_reset_tokens`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+-- Error reading structure for table rewardsplus_2.permissions: #1932 - Table 'rewardsplus_2.permissions' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.permissions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`permissions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permission_names`
+--
+-- Error reading structure for table rewardsplus_2.permission_names: #1932 - Table 'rewardsplus_2.permission_names' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.permission_names: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`permission_names`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+-- Error reading structure for table rewardsplus_2.personal_access_tokens: #1932 - Table 'rewardsplus_2.personal_access_tokens' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`personal_access_tokens`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_errors`
+--
+-- Error reading structure for table rewardsplus_2.project_errors: #1932 - Table 'rewardsplus_2.project_errors' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.project_errors: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`project_errors`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+-- Error reading structure for table rewardsplus_2.roles: #1932 - Table 'rewardsplus_2.roles' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.roles: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`roles`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_has_permissions`
+--
+-- Error reading structure for table rewardsplus_2.role_has_permissions: #1932 - Table 'rewardsplus_2.role_has_permissions' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.role_has_permissions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`role_has_permissions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stores`
+--
+-- Error reading structure for table rewardsplus_2.stores: #1932 - Table 'rewardsplus_2.stores' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.stores: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`stores`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `store_details`
+--
+-- Error reading structure for table rewardsplus_2.store_details: #1932 - Table 'rewardsplus_2.store_details' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.store_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`store_details`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table rewardsplus_2.users: #1932 - Table 'rewardsplus_2.users' doesn't exist in engine
+-- Error reading data for table rewardsplus_2.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `rewardsplus_2`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
